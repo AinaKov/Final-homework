fff015c262ae1eb9da2edce84df6ce1edd72dbd7
AlinaKov <alkovkova@gmail.com>
2022-09-21 13:57:34 +0500

    конечный вид

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
-﻿string [,] Array = {{ "hello", "2", @"world", ":-)" },
-{"1234", "1567", "-2", "computer"},
-{ "Russian", "Denmark", "Kazan", "Tumen"}};
-int count = Array.Length;
+﻿string[,] tab = {{ "hello", "2", @"world", ":-)" },
+                    {"1234", "1567", "-2", "computer"},
+                    { "Russian", "Denmark", "Kazan", "Tumen"}};
 int n = 3;
+for (int i = 0; i < tab.GetLength(0); i++)
+{
+    Console.Write("[");
+    for (int j = 0; j < tab.GetLength(1); j++)
+    
+    {
+        if (tab[i, j].Length <= n)
+            Console.Write(tab[i, j] + ", ");
+
+    }
+
+    Console.WriteLine("]");
+}
 
