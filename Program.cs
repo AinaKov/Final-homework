ebf369314872428e95ddffab6ce9f1a7ba142eb2
AlinaKov <alkovkova@gmail.com>
2022-09-21 09:41:36 +0500

    Added count and n

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿string[,] Array = {{ "hello", "2", @"world", ":-)" },
+﻿string [,] Array = {{ "hello", "2", @"world", ":-)" },
 {"1234", "1567", "-2", "computer"},
 { "Russian", "Denmark", "Kazan", "Tumen"}};
+int count = Array.Length;
+int n = 3;
 
